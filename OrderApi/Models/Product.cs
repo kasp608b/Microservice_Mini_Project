3749f85f65c7e9bb3030fb72db1654272bc0c541
scupak <antonibujko@hotmail.com>
2023-03-03 09:25:28 +0100

    Update Product.cs

--- OrderApi/Models/Product.cs
+++ OrderApi/Models/Product.cs
@@ -6,6 +6,7 @@ namespace OrderApi.Models
         public int ProductId { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public string Category { get; set; }
         public int ItemsInStock { get; set; }
         public int ItemsReserved { get; set; }
     }
